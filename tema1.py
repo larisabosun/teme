93c5c34b1198704b484de0eaf70d313504826474
larisabosun <larisabosun@yahoo.com>
2022-09-18 21:14:10 +0100

    tema1

---
+++ tema1.py
@@ -0,0 +1,17 @@
+# declara o lista care contine: 7,8,9,2,3,1,4,10,5,6(aceeasi ordine)
+list1 = [7,8,9,2,3,1,4,10,5,6]
+print(list1)
+list1.sort()
+print(list1)
+list1.reverse()
+print(list1)
+list2 = list1[-4:]
+print(list2)
+list2=list1[0:9:2]
+print(list2)
+list3=list1[1:11:2]
+print(list3)
+#afiseaza o lista ce contine numerele ce sunt multipli ai lui 3 (folosind slice)
+list4=list1[]
+print(list4)
+
